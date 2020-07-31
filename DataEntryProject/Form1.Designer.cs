7000273525f6309b5ac4e0c0099325660265834f
Yacine ch <chix88@gmail.com>
2020-07-31 03:27:46 +0200

    Ajoutez des fichiers projet.

---
+++ DataEntryProject/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace DataEntryProject
+{
+    partial class frmDataEntry
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grbDataEntry = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbTimer = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.timTimer = new System.Windows.Forms.Timer(this.components);
+            this.grbDataEntry.SuspendLayout();
+            this.grbTimer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbDataEntry
+            // 
+            this.grbDataEntry.Controls.Add(this.btnClear);
+            this.grbDataEntry.Controls.Add(this.btnAccept);
+            this.grbDataEntry.Controls.Add(this.txtName);
+            this.grbDataEntry.Controls.Add(this.txtAdress);
+            this.grbDataEntry.Controls.Add(this.txtCity);
+            this.grbDataEntry.Controls.Add(this.txtState);
+            this.grbDataEntry.Controls.Add(this.txtZip);
+            this.grbDataEntry.Controls.Add(this.label5);
+            this.grbDataEntry.Controls.Add(this.label4);
+            this.grbDataEntry.Controls.Add(this.label3);
+            this.grbDataEntry.Controls.Add(this.label2);
+            this.grbDataEntry.Controls.Add(this.label1);
+            this.grbDataEntry.Location = new System.Drawing.Point(68, 68);
+            this.grbDataEntry.Name = "grbDataEntry";
+            this.grbDataEntry.Size = new System.Drawing.Size(200, 289);
+            this.grbDataEntry.TabIndex = 0;
+            this.grbDataEntry.TabStop = false;
+            this.grbDataEntry.Text = "DataEntry";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(115, 231);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            this.btnClear.MouseLeave += new System.EventHandler(this.BtnAccept_MouseLeave);
+            this.btnClear.MouseHover += new System.EventHandler(this.btnAccept_Hover);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(15, 231);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 5;
+            this.btnAccept.Text = "&Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.BtnAccept_Click);
+            this.btnAccept.MouseLeave += new System.EventHandler(this.BtnAccept_MouseLeave);
+            this.btnAccept.MouseHover += new System.EventHandler(this.btnAccept_Hover);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(66, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(124, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(66, 64);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(124, 20);
+            this.txtAdress.TabIndex = 1;
+            this.txtAdress.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(66, 98);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(124, 20);
+            this.txtCity.TabIndex = 2;
+            this.txtCity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(66, 133);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(124, 20);
+            this.txtState.TabIndex = 3;
+            this.txtState.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(66, 166);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(124, 20);
+            this.txtZip.TabIndex = 4;
+            this.txtZip.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zip";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adress";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // grbTimer
+            // 
+            this.grbTimer.Controls.Add(this.btnExit);
+            this.grbTimer.Controls.Add(this.btnPause);
+            this.grbTimer.Controls.Add(this.btnStart);
+            this.grbTimer.Controls.Add(this.txtTimer);
+            this.grbTimer.Location = new System.Drawing.Point(317, 68);
+            this.grbTimer.Name = "grbTimer";
+            this.grbTimer.Size = new System.Drawing.Size(200, 289);
+            this.grbTimer.TabIndex = 1;
+            this.grbTimer.TabStop = false;
+            this.grbTimer.Text = "Timer";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(61, 231);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(61, 116);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 8;
+            this.btnPause.Text = "&Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(61, 71);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimer.Location = new System.Drawing.Point(50, 37);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.ReadOnly = true;
+            this.txtTimer.Size = new System.Drawing.Size(100, 22);
+            this.txtTimer.TabIndex = 0;
+            this.txtTimer.Text = "00:00:00";
+            this.txtTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timTimer
+            // 
+            this.timTimer.Interval = 1000;
+            this.timTimer.Tick += new System.EventHandler(this.TimTimer_Tick);
+            // 
+            // frmDataEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.grbTimer);
+            this.Controls.Add(this.grbDataEntry);
+            this.Name = "frmDataEntry";
+            this.Text = "Data Entery Form";
+            this.grbDataEntry.ResumeLayout(false);
+            this.grbDataEntry.PerformLayout();
+            this.grbTimer.ResumeLayout(false);
+            this.grbTimer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbDataEntry;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbTimer;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Timer timTimer;
+    }
+}
+
---
+++ DataEntryProject/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntryProject
+{
+    public partial class frmDataEntry : Form
+    {
+        private TimeSpan elapsedTime;
+        private DateTime lastElapsed;
+
+        public frmDataEntry()
+        {
+           
+            InitializeComponent();
+            grbDataEntry.Enabled = false;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();   
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtAdress.Clear();
+            txtCity.Clear();
+            txtName.Clear();
+            txtState.Clear();
+            txtZip.Clear();
+            
+            txtName.Focus();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+            grbDataEntry.Enabled = true;
+            txtName.Focus();
+
+            timTimer.Enabled = true;
+            lastElapsed = DateTime.Now;
+            
+            
+        }
+
+        private void TimTimer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime += DateTime.Now - lastElapsed;
+            lastElapsed = DateTime.Now;
+
+            txtTimer.Text = Convert.ToString(new TimeSpan(elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds));
+            
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+
+            timTimer.Enabled = false;
+            grbDataEntry.Enabled = false;
+        }
+
+        private void BtnAccept_Click(object sender, EventArgs e)
+        {
+            string DataEntry;
+
+            if (txtName.Text.Equals("") || txtAdress.Text.Equals("") || txtCity.Text.Equals("") ||
+                txtState.Text.Equals("") || txtZip.Text.Equals(""))
+            {
+                MessageBox.Show("Each Box Have To Be Filled","Invalid Input",MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                txtName.Focus();
+                return;
+            }
+
+            DataEntry = txtName.Text + "\r\n" + txtCity.Text + "\r\n" + txtState.Text + "\r\n" + txtZip.Text + "\r\n" +
+                        txtAdress.Text;
+            MessageBox.Show(DataEntry);
+
+            btnClear.PerformClick();
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string txtBoxSender = ((TextBox) sender).Name;
+
+            if (e.KeyChar == 13)
+            {
+                switch (txtBoxSender)
+                {
+                    case "txtName":
+                        txtAdress.Focus();
+                        break;
+                    case "txtAdress":
+                        txtCity.Focus();
+                        break;
+                    case "txtCity":
+                        txtState.Focus();
+                        break;
+                    case "txtState":
+                        txtZip.Focus();
+                        break;
+                    case "txtZip":
+                        btnAccept.Focus();
+                        break;
+
+                }
+                e.Handled = true; // for deletng the sound of the Enter press
+                
+            }
+
+            if (txtBoxSender == "txtZip")
+            {
+                if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == 8)
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnAccept_Hover(object sender, EventArgs e)
+        {
+            Color cl = Color.OrangeRed;
+            Button acceptButton = ((Button) sender);
+            acceptButton.BackColor = cl;
+            
+        }
+
+        private void BtnAccept_MouseLeave(object sender, EventArgs e)
+        {
+            Button acceptButton = ((Button)sender);
+            acceptButton.BackColor = DefaultBackColor;
+        }
+    }
+}
